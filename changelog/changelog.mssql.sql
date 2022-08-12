7d11f53980ebca1127f3b89cc897d65d6c1b562b
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2022-08-12 09:23:26 +0100

    feat(): small updates

--- changelog/changelog.mssql.sql
+++ changelog/changelog.mssql.sql
@@ -1,4 +1,12 @@
 -- liquibase formatted sql
 
 -- changeset jhderojas:1
-CREATE TABLE Todo (id int IDENTITY(1,1) NOT NULL, name varchar(100) NOT NULL, done NUMBER(1))
+CREATE TABLE [dbo].[Todo](
+	[id] [uniqueidentifier] NOT NULL,
+	[description] [nchar](100) NOT NULL,
+	[completed] [bit] NOT NULL,
+	[date] [bigint] NOT NULL
+) ON [PRIMARY]
+
+-- changeset jhderojas:2
+ALTER TABLE [dbo].[Todo] ADD  CONSTRAINT [DF_Todo_completed]  DEFAULT ((0)) FOR [completed]
